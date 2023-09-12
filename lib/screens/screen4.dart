8ae3963723609e2954f0c3c004f3dee1cac0e7d4
TAMILMANI1234 <81792499+TAMILMANI1234@users.noreply.github.com>
2023-09-12 12:00:42 +0530

    Update screen4.dart

--- lib/screens/screen4.dart
+++ lib/screens/screen4.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:table_calendar/table_calendar.dart';
+
 class Screen4 extends StatefulWidget {
   const Screen4({super.key});
 
@@ -19,14 +21,17 @@ class _Screen4State extends State<Screen4> {
   void showToast(){
     Fluttertoast.showToast(
         msg: "Message from Toast ",
-        backgroundColor: Colors.grey,
         fontSize: 20.0,
         textColor: Colors.white ,
         toastLength: Toast.LENGTH_LONG,
-        gravity: ToastGravity.BOTTOM,
+        gravity: ToastGravity.TOP,
         timeInSecForIosWeb:1
     );
   }
+
+
+  //////////////////////calender///////////////////
+  DateTime today= DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +40,7 @@ class _Screen4State extends State<Screen4> {
         title: const Text("Screen"),
         backgroundColor: Colors.red,
       ),
-     /* body: Column(
+    /* body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Myslide(),
@@ -65,6 +70,8 @@ class _Screen4State extends State<Screen4> {
           )
         ),
       )*/
+
+      /////////////////////grid////////////////////////////////////
      /* body: Padding(
         padding: EdgeInsets.all(20.0),
         child: GridView.count(
@@ -96,20 +103,129 @@ class _Screen4State extends State<Screen4> {
 
         ),
       )*/
+    /*  body: Center(
+        child: ElevatedButton(
+          onPressed:showToast ,
+          child: Text("Click"),
+        ),
+      ),*/
+
+
+      /////////////// Calender //////////////////////////////////////
+      /*body: Column(
+        children: [
+          Text("123"),
+          Container(
+            child: TableCalendar(
+              focusedDay:today,
+              firstDay: DateTime.utc(2010,10,16),
+              lastDay: DateTime.utc(2040,3,13),
+            ),
+          )
+        ],
+      ),*/
+      
+      
+     /* body: Center(
+        child: Column(
+          children: [
+            DataTable(
+                columns:[
+                  DataColumn(label: Text("S.no",style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red
+                  ),)),
+                  DataColumn(label: Text("Name",style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red
+                  ))),
+                  DataColumn(label: Text("Age",style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red
+                  ))),
+                  DataColumn(label: Text("Mobile No",style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red
+                  ))),
+
+                ],
+                rows:[
+                  DataRow(cells: [
+                    DataCell(Text("1")),
+                    DataCell(Text("Harris")),
+                    DataCell(Text("25")),
+                    DataCell(Text("7010416422")),
+
+                  ]),
+
+                  DataRow(cells: [
+                    DataCell(Text("2")),
+                    DataCell(Text("Harris")),
+                    DataCell(Text("25")),
+                    DataCell(Text("7010416422")),
+
+                  ]),
+
+                  DataRow(cells: [
+                    DataCell(Text("3")),
+                    DataCell(Text("Harris")),
+                    DataCell(Text("25")),
+                    DataCell(Text("7010416422")),
+
+
+                  ]),
+
+                  DataRow(cells: [
+                    DataCell(Text("4")),
+                    DataCell(Text("Harris")),
+                    DataCell(Text("25")),
+                    DataCell(Text("7010416422")),
+
+                  ]),
+                ]
+            )
+          ],
+        ),
+      )*/
+      /*body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(
+              backgroundColor: Colors.blue,
+              valueColor: AlwaysStoppedAnimation(Colors.green),
+              strokeWidth: 5,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            LinearProgressIndicator(
+              backgroundColor: Colors.blue,
+              valueColor: AlwaysStoppedAnimation(Colors.green),
+              minHeight: 10,
+            )
+          ],
+        ),
+      ),*/
       body: Center(
         child: ElevatedButton(
-          onPressed:(){
-            Fluttertoast.showToast(
-                msg: "Message from Toast ",
-                backgroundColor: Colors.grey,
-                fontSize: 20.0,
-                textColor: Colors.white ,
-                toastLength: Toast.LENGTH_LONG,
-                gravity: ToastGravity.BOTTOM,
-
+          onPressed: (){
+            final snackBar = SnackBar(
+              content: const Text('This is SnackBar!'),
+              backgroundColor: (Colors.black12),
+              action: SnackBarAction(
+                label: 'dismiss',
+                onPressed: () {
+                },
+              ),
             );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
           },
-          child: Text("Click"),
+          child: Text("Click to display Snackbar"),
         ),
       ),
     );
@@ -142,13 +258,6 @@ class _Screen4State extends State<Screen4> {
            ),
          ),
        )
-
-    );
-  }
-
-  Container blur(){
-    return Container(
-      child: const Text("Blur")
 
     );
   }
